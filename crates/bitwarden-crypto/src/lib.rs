ceba460a14d80b37cd2446d5a63135d95390ef1e
Daniel García <dani-garcia@users.noreply.github.com>
2024-12-06 19:22:21 +0100

    Replace ZeroizingAllocator (#38)
    ## 🎟️ Tracking
    
    <!-- Paste the link to the Jira or GitHub issue or otherwise describe /
    point to where this change is coming from. -->
    
    ## 📔 Objective
    
    Replace our internal ZeroizingAllocator by the external crate
    `zeroizing-alloc`, which has the same functionality but removes some
    undefined behavior.
    
    ## ⏰ Reminders before review
    
    - Contributor guidelines followed
    - All formatters and local linters executed and passed
    - Written new unit and / or integration tests where applicable
    - Protected functional changes with optionality (feature flags)
    - Used internationalization (i18n) for all UI strings
    - CI builds passed
    - Communicated to DevOps any deployment requirements
    - Updated any necessary documentation (Confluence, contributing docs) or
    informed the documentation
      team
    
    ## 🦮 Reviewer guidelines
    
    <!-- Suggested interactions but feel free to use (or not) as you desire!
    -->
    
    - 👍 (`:+1:`) or similar for great changes
    - 📝 (`:memo:`) or ℹ️ (`:information_source:`) for notes or general info
    - ❓ (`:question:`) for questions
    - 🤔 (`:thinking:`) or 💭 (`:thought_balloon:`) for more open inquiry
    that's not quite a confirmed
      issue and could potentially benefit from discussion
    - 🎨 (`:art:`) for suggestions / improvements
    - ❌ (`:x:`) or ⚠️ (`:warning:`) for more significant problems or
    concerns needing attention
    - 🌱 (`:seedling:`) or ♻️ (`:recycle:`) for future improvements or
    indications of technical debt
    - ⛏ (`:pick:`) for minor or nitpick changes

--- crates/bitwarden-crypto/src/lib.rs
+++ crates/bitwarden-crypto/src/lib.rs
@@ -80,8 +80,7 @@ mod util;
 pub use util::{generate_random_alphanumeric, generate_random_bytes, pbkdf2};
 mod wordlist;
 pub use wordlist::EFF_LONG_WORD_LIST;
-mod allocator;
-pub use allocator::ZeroizingAllocator;
+pub use zeroizing_alloc::ZeroAlloc as ZeroizingAllocator;
 
 #[cfg(feature = "uniffi")]
 uniffi::setup_scaffolding!();
