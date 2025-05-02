a05fd36d9cef27738f6f8b72eda6b2aebc015afe
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2025-05-02 04:33:45 -0700

    Update encryption_settings.rs

--- crates/bitwarden-core/src/client/encryption_settings.rs
+++ crates/bitwarden-core/src/client/encryption_settings.rs
@@ -1,127 +1,226 @@
-use bitwarden_crypto::{AsymmetricCryptoKey, KeyStore, SymmetricCryptoKey};
+use std::sync::{Arc, RwLock};
+
+use bitwarden_crypto::KeyStore;
+#[cfg(any(feature = "internal", feature = "secrets"))]
+use bitwarden_crypto::SymmetricCryptoKey;
 #[cfg(feature = "internal")]
-use bitwarden_crypto::{EncString, UnsignedSharedKey};
-use bitwarden_error::bitwarden_error;
-use thiserror::Error;
+use bitwarden_crypto::{EncString, Kdf, MasterKey, PinKey, UnsignedSharedKey};
+use chrono::Utc;
 use uuid::Uuid;
 
+#[cfg(feature = "secrets")]
+use super::login_method::ServiceAccountLoginMethod;
 use crate::{
-    key_management::{AsymmetricKeyId, KeyIds, SymmetricKeyId},
-    MissingPrivateKeyError, VaultLockedError,
+    auth::renew::renew_token,
+    client::{encryption_settings::EncryptionSettings, login_method::LoginMethod},
+    key_management::KeyIds,
+    DeviceType,
+};
+#[cfg(feature = "internal")]
+use crate::{
+    client::encryption_settings::EncryptionSettingsError,
+    client::{flags::Flags, login_method::UserLoginMethod},
+    error::NotAuthenticatedError,
 };
 
-#[bitwarden_error(flat)]
-#[derive(Debug, Error)]
-pub enum EncryptionSettingsError {
-    #[error("Cryptography error, {0}")]
-    Crypto(#[from] bitwarden_crypto::CryptoError),
-
-    #[error(transparent)]
-    InvalidBase64(#[from] base64::DecodeError),
-
-    #[error(transparent)]
-    VaultLocked(#[from] VaultLockedError),
-
-    #[error("Invalid private key")]
-    InvalidPrivateKey,
-
-    #[error(transparent)]
-    MissingPrivateKey(#[from] MissingPrivateKeyError),
+#[derive(Debug, Clone)]
+pub struct ApiConfigurations {
+    pub identity: bitwarden_api_identity::apis::configuration::Configuration,
+    pub api: bitwarden_api_api::apis::configuration::Configuration,
+    pub device_type: DeviceType,
 }
 
-pub struct EncryptionSettings {}
+#[derive(Debug, Default, Clone)]
+pub(crate) struct Tokens {
+    // These two fields are always written to, but they are not read
+    // from the secrets manager SDK.
+    #[cfg_attr(not(feature = "internal"), allow(dead_code))]
+    access_token: Option<String>,
+    pub(crate) expires_on: Option<i64>,
 
-impl EncryptionSettings {
-    /// Initialize the encryption settings with the decrypted user key and the encrypted user
-    /// private key This should only be used when unlocking the vault via biometrics or when the
-    /// vault is set to lock: "never" Otherwise handling the decrypted user key is dangerous and
-    /// discouraged
+    #[cfg_attr(not(feature = "internal"), allow(dead_code))]
+    pub(crate) refresh_token: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct InternalClient {
+    pub(crate) tokens: RwLock<Tokens>,
+    pub(crate) login_method: RwLock<Option<Arc<LoginMethod>>>,
+
     #[cfg(feature = "internal")]
-    pub(crate) fn new_decrypted_key(
+    pub(super) flags: RwLock<Flags>,
+
+    /// Use Client::get_api_configurations().await to access this.
+    /// It should only be used directly in renew_token
+    #[doc(hidden)]
+    pub(crate) __api_configurations: RwLock<Arc<ApiConfigurations>>,
+
+    /// Reqwest client useable for external integrations like email forwarders, HIBP.
+    #[allow(unused)]
+    pub(crate) external_client: reqwest::Client,
+
+    pub(super) key_store: KeyStore<KeyIds>,
+}
+
+impl InternalClient {
+    #[cfg(feature = "internal")]
+    pub fn load_flags(&self, flags: std::collections::HashMap<String, bool>) {
+        *self.flags.write().expect("RwLock is not poisoned") = Flags::load_from_map(flags);
+    }
+
+    #[cfg(feature = "internal")]
+    pub fn get_flags(&self) -> Flags {
+        self.flags.read().expect("RwLock is not poisoned").clone()
+    }
+
+    #[cfg(feature = "internal")]
+    pub(crate) fn get_login_method(&self) -> Option<Arc<LoginMethod>> {
+        self.login_method
+            .read()
+            .expect("RwLock is not poisoned")
+            .clone()
+    }
+
+    pub fn get_access_token_organization(&self) -> Option<Uuid> {
+        match self
+            .login_method
+            .read()
+            .expect("RwLock is not poisoned")
+            .as_deref()
+        {
+            #[cfg(feature = "secrets")]
+            Some(LoginMethod::ServiceAccount(ServiceAccountLoginMethod::AccessToken {
+                organization_id,
+                ..
+            })) => Some(*organization_id),
+            _ => None,
+        }
+    }
+
+    #[cfg(any(feature = "internal", feature = "secrets"))]
+    pub(crate) fn set_login_method(&self, login_method: LoginMethod) {
+        use log::debug;
+
+        debug! {"setting login method: {:#?}", login_method}
+        *self.login_method.write().expect("RwLock is not poisoned") = Some(Arc::new(login_method));
+    }
+
+    pub(crate) fn set_tokens(&self, token: String, refresh_token: Option<String>, expires_in: u64) {
+        *self.tokens.write().expect("RwLock is not poisoned") = Tokens {
+            access_token: Some(token.clone()),
+            expires_on: Some(Utc::now().timestamp() + expires_in as i64),
+            refresh_token,
+        };
+        let mut guard = self
+            .__api_configurations
+            .write()
+            .expect("RwLock is not poisoned");
+
+        let inner = Arc::make_mut(&mut guard);
+        inner.identity.oauth_access_token = Some(token.clone());
+        inner.api.oauth_access_token = Some(token);
+    }
+
+    #[cfg(feature = "internal")]
+    pub fn is_authed(&self) -> bool {
+        let is_token_set = self
+            .tokens
+            .read()
+            .expect("RwLock is not poisoned")
+            .access_token
+            .is_some();
+        let is_login_method_set = self
+            .login_method
+            .read()
+            .expect("RwLock is not poisoned")
+            .is_some();
+
+        is_token_set || is_login_method_set
+    }
+
+    #[cfg(feature = "internal")]
+    pub fn get_kdf(&self) -> Result<Kdf, NotAuthenticatedError> {
+        match self
+            .login_method
+            .read()
+            .expect("RwLock is not poisoned")
+            .as_deref()
+        {
+            Some(LoginMethod::User(
+                UserLoginMethod::Username { kdf, .. } | UserLoginMethod::ApiKey { kdf, .. },
+            )) => Ok(kdf.clone()),
+            _ => Err(NotAuthenticatedError),
+        }
+    }
+
+    pub async fn get_api_configurations(&self) -> Arc<ApiConfigurations> {
+        // At the moment we ignore the error result from the token renewal, if it fails,
+        // the token will end up expiring and the next operation is going to fail anyway.
+        renew_token(self).await.ok();
+        self.__api_configurations
+            .read()
+            .expect("RwLock is not poisoned")
+            .clone()
+    }
+
+    #[cfg(feature = "internal")]
+    pub fn get_http_client(&self) -> &reqwest::Client {
+        &self.external_client
+    }
+
+    pub fn get_key_store(&self) -> &KeyStore<KeyIds> {
+        &self.key_store
+    }
+
+    #[cfg(feature = "internal")]
+    pub(crate) fn initialize_user_crypto_master_key(
+        &self,
+        master_key: MasterKey,
+        user_key: EncString,
+        private_key: EncString,
+    ) -> Result<(), EncryptionSettingsError> {
+        let user_key = master_key.decrypt_user_key(user_key)?;
+        EncryptionSettings::new_decrypted_key(user_key, private_key, &self.key_store)?;
+
+        Ok(())
+    }
+
+    #[cfg(feature = "internal")]
+    pub(crate) fn initialize_user_crypto_decrypted_key(
+        &self,
         user_key: SymmetricCryptoKey,
         private_key: EncString,
-        store: &KeyStore<KeyIds>,
     ) -> Result<(), EncryptionSettingsError> {
-        use bitwarden_crypto::KeyDecryptable;
-        use log::warn;
-
-        use crate::key_management::{AsymmetricKeyId, SymmetricKeyId};
-
-        let private_key = {
-            let dec: Vec<u8> = private_key.decrypt_with_key(&user_key)?;
-
-            // FIXME: [PM-11690] - Temporarily ignore invalid private keys until we have a recovery
-            // process in place.
-            AsymmetricCryptoKey::from_der(&dec)
-                .map_err(|_| {
-                    warn!("Invalid private key");
-                })
-                .ok()
-
-            // Some(
-            //     AsymmetricCryptoKey::from_der(&dec)
-            //         .map_err(|_| EncryptionSettingsError::InvalidPrivateKey)?,
-            // )
-        };
-
-        // FIXME: [PM-18098] When this is part of crypto we won't need to use deprecated methods
-        #[allow(deprecated)]
-        {
-            let mut ctx = store.context_mut();
-            ctx.set_symmetric_key(SymmetricKeyId::User, user_key)?;
-            if let Some(private_key) = private_key {
-                ctx.set_asymmetric_key(AsymmetricKeyId::UserPrivateKey, private_key)?;
-            }
-        }
+        EncryptionSettings::new_decrypted_key(user_key, private_key, &self.key_store)?;
 
         Ok(())
-    }
-
-    /// Initialize the encryption settings with only a single decrypted organization key.
-    /// This is used only for logging in Secrets Manager with an access token
-    #[cfg(feature = "secrets")]
-    pub(crate) fn new_single_org_key(
-        organization_id: Uuid,
-        key: SymmetricCryptoKey,
-        store: &KeyStore<KeyIds>,
-    ) {
-        // FIXME: [PM-18098] When this is part of crypto we won't need to use deprecated methods
-        #[allow(deprecated)]
-        store
-            .context_mut()
-            .set_symmetric_key(SymmetricKeyId::Organization(organization_id), key)
-            .expect("Mutable context");
     }
 
     #[cfg(feature = "internal")]
-    pub(crate) fn set_org_keys(
-        org_enc_keys: Vec<(Uuid, UnsignedSharedKey)>,
-        store: &KeyStore<KeyIds>,
+    pub(crate) fn initialize_user_crypto_pin(
+        &self,
+        pin_key: PinKey,
+        pin_protected_user_key: EncString,
+        private_key: EncString,
     ) -> Result<(), EncryptionSettingsError> {
-        let mut ctx = store.context_mut();
+        let decrypted_user_key = pin_key.decrypt_user_key(pin_protected_user_key)?;
+        self.initialize_user_crypto_decrypted_key(decrypted_user_key, private_key)
+    }
 
-        // FIXME: [PM-11690] - Early abort to handle private key being corrupt
-        if org_enc_keys.is_empty() {
-            return Ok(());
-        }
+    #[cfg(feature = "secrets")]
+    pub(crate) fn initialize_crypto_single_org_key(
+        &self,
+        organization_id: Uuid,
+        key: SymmetricCryptoKey,
+    ) {
+        EncryptionSettings::new_single_org_key(organization_id, key, &self.key_store);
+    }
 
-        if !ctx.has_asymmetric_key(AsymmetricKeyId::UserPrivateKey) {
-            return Err(MissingPrivateKeyError.into());
-        }
-
-        // Make sure we only keep the keys given in the arguments and not any of the previous
-        // ones, which might be from organizations that the user is no longer a part of anymore
-        ctx.retain_symmetric_keys(|key_ref| !matches!(key_ref, SymmetricKeyId::Organization(_)));
-
-        // Decrypt the org keys with the private key
-        for (org_id, org_enc_key) in org_enc_keys {
-            ctx.decapsulate_key_unsigned(
-                AsymmetricKeyId::UserPrivateKey,
-                SymmetricKeyId::Organization(org_id),
-                &org_enc_key,
-            )?;
-        }
-
-        Ok(())
+    #[cfg(feature = "internal")]
+    pub fn initialize_org_crypto(
+        &self,
+        org_keys: Vec<(Uuid, UnsignedSharedKey)>,
+    ) -> Result<(), EncryptionSettingsError> {
+        EncryptionSettings::set_org_keys(org_keys, &self.key_store)
     }
 }
